84b2181478c6b5ed68bd0467425d2125c0ea2d53
Noemie-P <77014708+Noemie-P@users.noreply.github.com>
2022-02-20 17:31:47 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,124 @@
+var express = require("express");
+var app = express();
+const morgan =require("morgan");
+var cors = require("cors");
+const QuestionnaireController = require("./controllers/questionnaire_controller");
+const userController = require("./controllers/user_controller");
+const answerController = require("./controllers/answer_controller");
+const categoryController = require("./controllers/category_controller");
+const groupController = require("./controllers/group_controller");
+var auth = require("./config/auth")
+var Questionnaire = require("./models/questionnaire_models.js");
+
+//var corsOptions = {
+    //origin: "http://localhost:8080"
+//};
+
+//app.use(express.json());
+app.use(express.json({limit: '10mb', extended: true}));
+app.use(express.urlencoded({limit: '10mb', extended: true}));
+//app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+app.use(morgan("dev"));
+
+//partie questionnaire
+app.post ("/creerQuestionnaire", function(req, res){
+    console.log("page créer");
+    QuestionnaireController.createQuestionnaire(req, res);
+});
+
+app.post ("/createExam", function(req, res){
+    console.log("page créer");
+    QuestionnaireController.createExam(req, res);
+});
+
+app.get("/getQuestionsFromCategory/:id", QuestionnaireController.getQuestionsFromCategory);
+app.delete("/deleteQuestion/:id", QuestionnaireController.deleteQuestion);
+
+app.post ("/creerQuestions", function(req, res){
+    console.log("page créer");
+    QuestionnaireController.createQuestions(req, res);
+})
+
+app.get("/getQuestion/:id", QuestionnaireController.getQuestion);
+app.put("/updateQuestion/:id", QuestionnaireController.updateQuestion);
+
+app.post ("/creercategorie", function(req, res){
+    console.log("page créer");
+    categoryController.createCategory(req, res);
+})
+
+app.post ("/faireQuestionnaire", function(req, res,next){
+    console.log("page questionnaire");
+   QuestionnaireController.findQuestions(req, res,next);
+    
+})
+app.get ("/getQuestionnaire", function(req, res){
+    console.log("page get questionnaire");
+    Questionnaire.find()
+    .then(data => {
+      res.json(data);
+  })
+  .catch(err => {
+      res
+          .status(500)
+          .send({ message: "Error finding Users"  });
+          console.log(err)
+  });
+    
+});
+
+app.delete("/deleteQuestionnaire/:id", QuestionnaireController.deleteQuestionnaire);
+
+//partie categorie
+
+app.get ("/getCategory", function(req, res){
+    console.log("page get category");
+    categoryController.getCategory(req, res);
+  });
+
+app.get("/getCategory/:id", categoryController.getOneCategory);
+app.put("/updateCategory/:id", categoryController.updateCategory);
+app.delete("/deleteCategory/:id", categoryController.deleteCategory);
+
+app.post("/validerCode", function(req,res, next){
+    console.log("page code validé");
+    QuestionnaireController.validerCode(req,res, next);
+})
+//partie answers
+
+app.get("/getAnswers/:id", answerController.getAnswers);
+app.get("/getAllAnswers", answerController.getAllAnswers);
+app.post ("/saveAnswer", answerController.saveAnswer);
+app.get("/getUsersFromAnswers/:id", answerController.getUsersFromAnswers);
+app.put("/GradeQuestionnaire", answerController.GradeQuestionnaire);
+
+
+// partie user
+app.post ("/register", userController.registerNewUser);
+app.post ("/login", userController.loginUser);
+app.get ("/me", auth,userController.getUserDetails);
+app.get("/getUsers", userController.getUsers);
+//app.get("/getUsersPopulate", userController.getUsersPopulate);
+app.get("/getOneUser/:id", userController.getOneUser);
+app.get("/getUsersFromGroup/:id", userController.getUsersFromGroup);
+app.delete("/deleteUser/:id", userController.deleteUser);
+app.put("/updateUser/:id", userController.updateUser);
+app.put("/logOutUser/:id", userController.logOutUser);
+
+//partie groups
+
+app.post ("/createGroup", groupController.createGroup);
+app.get("/getGroups", groupController.getGroups);
+app.get("/getOneGroup/:id", groupController.getOneGroup);
+app.delete("/deleteGroup/:id", groupController.deleteGroup);
+app.put("/updateGroup/:id", groupController.updateGroup);
+
+
+
+app.listen(3000);
+
+module.exports= app;
---
+++ server/models/user_models.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/connect");
+
+mongoose.connect('mongodb://'+ config.MONGODB_CREDENTIALS +'/success',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à User réussie !'))
+  .catch(() => console.log('Connexion à User échouée !'));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    user_name: {
+      type:String,
+      required:true
+    },
+    type: {
+     type:String
+    },
+    groupe: {
+      type: Schema.Types.ObjectId, 
+      ref:'groups'
+    },
+    questionnaires: [
+      {
+        id_questionnaire:String,
+        note:Number,
+        date_passage:String,
+        passed:Boolean
+      }
+    ],
+    password: {
+      type: String,
+      required:true
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true
+        }
+      }
+    ]
+});
+
+//this method will hash the password before saving the user model
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+//this method generates an auth token for the user
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  //const token = jwt.sign({ _id: user._id, user_name: user.user_name, type: user.type}, "secret" );
+  const token = jwt.sign({user: user}, "secret" );
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+//this method search for a user by email and password.
+userSchema.statics.findByCredentials = async (user_name, password) => {
+  const user = await User.findOne({ user_name });
+  if (!user) {
+    throw new Error({ "error":"Invalid login details" })
+    console.log("error: Invalid user details" );
+  }
+  console.log (password);
+  console.log (user.password);
+  const isPasswordMatch = await bcrypt.compare( password, user.password);
+  console.log (isPasswordMatch);
+  if (!isPasswordMatch) {
+    throw new Error({ "error" : "Invalid login details" })
+    console.log( "error: Invalid password details" );
+  }
+  return user;
+};
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User;
